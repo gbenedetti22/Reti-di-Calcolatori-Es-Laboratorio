b85d6ba6e688d49ac6ad912bca26016ce5e9a6d2
gbenedetti22 <gabriele.benedetti22@gmail.com>
2021-09-26 21:02:38 +0200

    es 2 sistemato

--- ufficio_postale/src/com/unipi/Main.java
+++ ufficio_postale/src/com/unipi/Main.java
@@ -1,25 +1,36 @@
 package com.unipi;
 
+import java.util.LinkedList;
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final String[] persons = {"John", "Mike", "Gabriele", "Marco", "Matteo", "Topolino"};
+    private static final int N_THREAD_WORKERS = 2;
     private static final int N_PERSONS = 10;
+    private static final int K = 2;
 
     public static void main(String[] args) {
-        /* Per la richiesta facoltativa, basta usare un cachedThreadPool
-        *  oppure farsi un nuovo ThreadPoolExecutor specificando come queue la SynchronousQueue
-        *  e come tempo massimo, un valore arbitrario a scelta
-        * */
-        ExecutorService pool = Executors.newFixedThreadPool(4);
+        ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(K); //coda sala 2
+        LinkedList<Person> sala1 = new LinkedList<>();  //sala d'attesa
 
-        for (int i = 0; i < N_PERSONS; i++) {
-            int person = new Random().nextInt(persons.length);
+        ExecutorService pool = new ThreadPoolExecutor(N_THREAD_WORKERS, N_THREAD_WORKERS,
+                0L, TimeUnit.MILLISECONDS,
+                queue);
 
-            pool.submit(new Person(persons[person]));
+        int servedPeople = 0;
+
+        while (servedPeople < N_PERSONS) {
+            Person person = new Person(persons[new Random().nextInt(persons.length)]); //persona con nome random
+            sala1.add(person);  // simulo l entrata della persona x all interno delle Poste
+
+            if(queue.size() != K){  //se la coda non è piena
+                pool.submit(sala1.poll());  //la persona x si mette in fila nella sala 2
+                servedPeople++;
+            }
         }
 
         pool.shutdown();
